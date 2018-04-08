75d2b5c0029ea34ac5790f8b7d5522464edc06f6
e <2586354329@qq.com>
2018-04-08 11:04:42 +0800

    初始导入

---
+++ src/main/java/com/xinhua/scsigncenter/ScSignCenterApplication.java
@@ -0,0 +1,12 @@
+package com.xinhua.scsigncenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScSignCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScSignCenterApplication.class, args);
+	}
+}
